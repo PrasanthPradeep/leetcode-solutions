a04729e3e214ea4f4356ae6d22b4988eda13a668
Prasanth P <programmerprasanth@gmail.com>
2026-07-22 16:44:13 +0000

    Sync LeetCode submission Runtime - 22 ms (34.22%), Memory - 19.3 MB (62.52%)

---
+++ 3206-find-common-elements-between-two-arrays/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        answer1, answer2 = 0, 0
+        for i in nums1:
+                if i in nums2:
+                    answer1 += 1
+        for i in nums2:
+                if i in nums1:
+                    answer2 += 1
+        return answer1, answer2
